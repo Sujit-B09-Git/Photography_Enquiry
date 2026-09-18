3c08298f8188dcd15e4b042f36de125d34419a08
Sujit-B09-Git <sujitbharate2003@gmail.com>
2026-09-18 11:51:20 +0530

    Setup Photography Enquiry project

--- dist/config.js
+++ dist/config.js
@@ -1,3 +1,3 @@
 window.SPF_CONFIG = {
-  googleScriptUrl: ""
+  googleScriptUrl: "https://script.google.com/macros/s/AKfycbznRwNi0m0RwkMX3OAp9b99p-fKfGZUopAVofrWoRmyZmDSPMxHT0mPZhazfug_r6Y/exec"
 };
